b4d97b047c4ca7c9233b0e0e45eb9928aa04d9b3
payaoanderson <andersonpayao018@gmail.com>
2024-11-13 22:13:39 -0300

    c

--- consultacnpj_anderson/lib/consulta_cnpj.dart
+++ consultacnpj_anderson/lib/consulta_cnpj.dart
@@ -6,7 +6,7 @@ class CnpjData {
   List<String>? emails;
   List<Member>? members;
   String? sideActivities;
-
+ 
   CnpjData({
     this.alias,
     this.founded,
@@ -16,22 +16,23 @@ class CnpjData {
     this.members,
     this.sideActivities,
   });
-
-  factory CnpjData.fromJSON(Map<String, dynamic> json) {
+ 
+  factory CnpjData.fromJson(Map<String, dynamic> json) {
     return CnpjData(
-      alias: json["alias"],
-      founded: json["founded"],
-      address: Address.fromJSON(json["address"]),
-      phones: List<String>.from(json["phones"].map((x) => x['area'] + x['number'])),
-      emails: List<String>.from(json["phones"].map((x) => x['address'])),
+      alias: json['alias'],
+      founded: json['founded'],
+      address: Address.fromJson(json['address']),
+      phones: List<String>.from(json['phones'].map((x) => x['area'] + x['number'])),
+      emails: List<String>.from(json['emails'].map((x) => x['address'])),
       members: (json['company']['members'] as List)
-      .map((member) => member.fromJSON(member)).toList().
-      sideActivities: json["sideActivities"]['Text'],
+          .map((member) => Member.fromJson(member))
+          .toList(),
+      sideActivities: json['sideActivities']['text'],
     );
   }
 }
-
-class Address{
+ 
+class Address {
   String? street;
   String? number;
   String? district;
@@ -39,8 +40,40 @@ class Address{
   String? state;
   String? country;
   String? zip;
+ 
+  Address({
+    this.street,
+    this.number,
+    this.district,
+    this.city,
+    this.state,
+    this.country,
+    this.zip,
+  });
+ 
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      street: json['street'],
+      number: json['number'],
+      district: json['district'],
+      city: json['city'],
+      state: json['state'],
+      country: json['country']['name'],
+      zip: json['zip'],
+    );
+  }
 }
-
-Address ({
- this.street,
-});
+ 
+class Member {
+  String? name;
+  int? age;
+ 
+  Member({this.name, this.age});
+ 
+  factory Member.fromJson(Map<String, dynamic> json) {
+    return Member(
+      name: json['name'],
+      age: json['age'],
+    );
+  }
+}
